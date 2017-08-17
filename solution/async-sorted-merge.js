cf5f878d16af4e0f12b76bec9095b74e1a6862ec
ewu302 <enmingdavidwu@gmail.com>
2017-08-17 02:16:44 -0400

    implemented solutions

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -1,5 +1,47 @@
 'use strict'
 
+const PriorityQueue = require('priorityqueuejs');
+const P = require('bluebird')
+
 module.exports = (logSources, printer) => {
-	throw new Error('Not implemented yet!  That part is up to you!')
+  let priorityQueue = new PriorityQueue((a, b) => {
+    return b.logEntry.date.getTime() - a.logEntry.date.getTime();
+  });
+
+  let initPromises = [];
+  logSources.forEach((logSource, index) => {
+    initPromises.push(logSource.popAsync().then((logEntry) => {
+      if (logEntry) {
+        let logEntryNode = {
+          logEntry: logEntry,
+          logSourcesInd: index
+        };
+        priorityQueue.enq(logEntryNode);
+      }
+    }));
+  });
+
+  P.all(initPromises).then(() => {
+    printAndEnqueueAllLogEntries().then(() => {
+      printer.done();
+    });
+  });
+
+  function printAndEnqueueAllLogEntries() {
+    let logEntryNode = priorityQueue.deq();
+    printer.print(logEntryNode.logEntry);
+
+    return logSources[logEntryNode.logSourcesInd].popAsync().then((newLogEntry) => {
+      if (newLogEntry) {
+        let newlogEntryNode = {
+          logEntry: newLogEntry,
+          logSourcesInd: logEntryNode.logSourcesInd
+        };
+        priorityQueue.enq(newlogEntryNode);
+      }
+      if (priorityQueue.size()) {
+        return printAndEnqueueAllLogEntries();
+      }
+    });
+  }
 }
--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -1,11 +1,35 @@
 'use strict'
 
+const PriorityQueue = require('priorityqueuejs');
+
 module.exports = (logSources, printer) => {
-  console.log(logSources);
-  printer.print(logSources[0].pop())
-  // do {
-  //   var test = logSources[0].pop();
-  //   console.log(test);
-  // } while (test);
-	throw new Error('Not implemented yet!  That part is up to you!')
+  let priorityQueue = new PriorityQueue((a, b) => {
+    return b.logEntry.date.getTime() - a.logEntry.date.getTime();
+  });
+
+  logSources.forEach((logSource, index) => {
+    let logEntry = logSource.pop();
+    if (logEntry) {
+      let logEntryNode = {
+        logEntry: logEntry,
+        logSourcesInd: index
+      };
+      priorityQueue.enq(logEntryNode);
+    }
+  });
+
+  while (priorityQueue.size()) {
+    let logEntryNode = priorityQueue.deq();
+    printer.print(logEntryNode.logEntry);
+
+    let newLogEntry = logSources[logEntryNode.logSourcesInd].pop();
+    if (newLogEntry) {
+      let newlogEntryNode = {
+        logEntry: newLogEntry,
+        logSourcesInd: logEntryNode.logSourcesInd
+      };
+      priorityQueue.enq(newlogEntryNode);
+    }
+  }
+  printer.done();
 }
